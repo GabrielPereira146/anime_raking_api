28c703e587a653d2a341792907612a8f52d4f334
GabrielPereira146 <gabriel.brown1112@gmail.com>
2024-05-01 11:01:28 -0300

    first commit

---
+++ src/main/java/com/project/api/anime_raking/AnimeRakingApplication.java
@@ -0,0 +1,13 @@
+package com.project.api.anime_raking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeRakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeRakingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/api/react/anime_raking/AnimeRakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.api.react.anime_raking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeRakingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
